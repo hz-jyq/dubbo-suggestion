f561a9e662ca3244f393673feb555e047a5da5f6
jinyuanqian <hzjyq@tairanchina.com>
2017-03-01 10:53:06 +0800

    add

--- src/main/java/com/fengdai/suggestion/dubbo/SuggestionService.java
+++ src/main/java/com/fengdai/suggestion/dubbo/SuggestionService.java
@@ -3,15 +3,32 @@ package com.fengdai.suggestion.dubbo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SuggestionService {
+
+	private static ClassPathXmlApplicationContext context = null;
+
+	static SuggestionService suggestionService = new SuggestionService();
+
+	static SuggestionService getInstance() {
+		return suggestionService;
+	}
 	
-	 public static void main(String[] args) throws Exception { 
-	    @SuppressWarnings("resource")
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config/suggestion-provider-dev.xml"});
-	 	System.out.println("-------------------DUBBO CONSUMER START-------------------");
+	public static void main(String[] args) throws Exception {
+		SuggestionService.getInstance().start();;
+		System.out.println("-------------------DUBBO CONSUMER START-------------------");
 		System.out.println("-------------------DUBBO CONSUMER END-------------------");
-		System.out.println("按任意键退出");
-	     context.start();
-	     System.in.read(); // 按任意键退出
-	    }
-	 
+		System.out.println("[SuggestionService SERVICE STARTED]");
+		System.in.read(); // 按任意键退出
+		while (true) {
+			Thread.sleep(1000);
+		}
+	}
+
+	@SuppressWarnings("resource")
+	public synchronized void start() {
+		if (context == null) {
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+					new String[] { "config/suggestion-provider-dev.xml" });
+			context.start();
+		}
+	}
 }
